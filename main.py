61c75f30f4fd739149be26a4f3eff14c913f8f95
citopes <jino0825@gmail.com>
2025-08-08 10:15:50 +0900

    requirements.txt add

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ if __name__ == "__main__":
         sys.exit(app.exec())
     else:
         sys.exit(0)
+
+
+# pip freeze > requirements.txt    현재 설치된 모듈 정보를 저장한다.
+# 향후 가상환경에서 pip install -r requirements.txt 입력. requirements.txt 파일 안에 정리된 리스트의 모든 가상환경을 설치한다.
